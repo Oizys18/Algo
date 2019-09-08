abac5c8a8e4f6dab6de5169637949324beb7398a
Oizys18 <oizys18@gmail.com>
2019-09-09 00:02:13 +0900

    mod

--- 190908/BJ15686.py
+++ 190908/BJ15686.py
@@ -14,7 +14,6 @@ def isPath(x,y):
     else:
         return False
 
-
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
@@ -34,33 +33,32 @@ def BFS(depth,x,y):
                         return dep + 1
                     queue.append((dep+1,nX,nY))
 
-house = []
+# house = []
 chicken = []
 for x in range(N):
     for y in range(N):
-        if mat[x][y] == 1:
-            house.append((x,y))
-        elif mat[x][y] == 2:
+        # if mat[x][y] == 1:
+        #     house.append((x,y))
+        if mat[x][y] == 2:
             chicken.append((x,y))
             # 치킨집 지워두기 
             mat[x][y] = 0
 
-comb = itertools.combinations(chicken,M)
-minRes = []
+minRes = 50000
 
-for survive in comb:
+for survive in itertools.combinations(chicken,M):
     for a,b in survive:
         mat[a][b] = 2
-    # print(mat)
     res = 0
-    for c,d in house:
-        # print(c,d)
-        res += BFS(0,c,d)
-        # print(res)
-    minRes.append(res)
+    for c in range(N):
+        for d in range(N):
+           if mat[c][d] == 1: 
+                res += BFS(0,c,d)
+    if res < minRes:
+        minRes = res
     for a,b in survive:
         mat[a][b] = 0
-print(min(minRes))
+print(minRes)
 
 
 
