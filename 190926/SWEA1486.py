3cfee1b673fc8a43d2fb0516672056ad023132eb
Oizys18 <oizys18@gmail.com>
2019-10-03 23:15:59 +0900

    new month

--- 190926/SWEA1486.py
+++
@@ -1,8 +0,0 @@
-# SWEA1486
-
-import sys 
-sys.stdin=open('input.txt','r')
-
-for T in range(int(input())):
-    N, B = map(int,input().split())
-    heights = [list(map(int,input().split()))]
--- 190930/SWEA5205.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open('input.txt','r')
-
-for T in range(int(input())):
-    N = int(input())
-    nums = list(map(int,input().split()))
-    print(sorted(nums)[N//2])
