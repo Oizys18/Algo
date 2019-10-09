bc64fb56ec3684257644a3b69745644c08ecef24
Oizys18 <oizys18@gmail.com>
2019-10-09 23:44:10 +0900

    mkdir programmers

---
+++ programmers/temp/02.py
@@ -0,0 +1,21 @@
+# 가운데 글자 가져오기 
+"""
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+입출력 예
+s	return
+abcde	c
+qwer	we
+"""
+def solution(s):
+    answer = ''
+    if len(s)%2:
+    #홀수
+        answer = s[len(s)//2]
+    else:
+    #짝수
+        answer = s[len(s)//2 - 1 :len(s)//2 + 1]
+        
+    return answer
