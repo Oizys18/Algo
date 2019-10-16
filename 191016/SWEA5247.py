f7f26f3e7276f0be14465d00cb274b5a039454fe
Oizys18 <Oizys18@gmail.com>
2019-10-16 18:00:21 +0900

    1016

---
+++ 191016/SWEA5247.py
@@ -0,0 +1,44 @@
+# 연산
+import sys
+sys.stdin = open('input6.txt','r')
+
+
+import collections
+
+
+
+for T in range(int(input())):
+    N, M = map(int,input().split())
+
+    def BFS(node, goal):
+        visit = [0]*1000001
+        queue = collections.deque([(0,node)])
+        while queue:
+            depth, node = queue.popleft() 
+            # print(depth, node)
+            if node > 1000000:
+                continue
+            else:
+                if visit[node]:
+                    continue
+                else:
+                    visit[node] = node
+                    if node * 2 == goal:
+                        return depth + 1
+                    else:
+                        queue.append((depth+1, node*2))
+                    if node + 1 == goal:
+                        return depth + 1
+                    else:
+                        queue.append((depth+1, node+1))
+                    if node - 10 == goal:
+                        return depth + 1
+                    else:
+                        queue.append((depth+1, node-10))
+                    
+                    if node - 1 == goal:
+                        return depth + 1
+                    else:
+                        queue.append((depth+1, node-1))
+                    
+    print(f"#{T+1} {BFS(N,M)}")
