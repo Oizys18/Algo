85c0481c7378902a749c04abb1382dc79bec7f88
Oizys18 <oizys18@gmail.com>
2019-10-26 23:33:40 +0900

    1026

--- 191025/SWEA5250.py
+++ 191025/SWEA5250.py
@@ -5,4 +5,32 @@ sys.stdin=open('5250.txt','r')
 for T in range(int(input())):
     N = int(input())
     mat = [list(map(int,input().split())) for _ in range(N)]
-    print(mat)
+    visit = [[0]*(N) for _ in range(N)]
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0] 
+
+
+    def isMap(x,y):
+        if 0 <= x <= (N+1) and 0 <= y <= (N+1):
+            if visit[x][y]:
+                return False
+            else:
+                return True
+        else:
+            return False
+
+    def BFS(depth,x,y):
+        queue = []
+        queue.append((depth,x,y))
+        while queue:
+            depth, x,y = queue.pop(0)
+            if not visit[x][y]:
+                visit[x][y] = 1
+                for i in range(4):
+                    newX = x + dx[i]
+                    newY = y + dy[i]
+                    if isMap(newX,newY):
+                        queue.append((depth+1,newX,newY))
+        
+    BFS(0,0,0)
+    
