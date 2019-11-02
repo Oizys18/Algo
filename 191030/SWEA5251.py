f3601bbebe104be2060a234e2760d93a0ee530cb
Oizys18 <oizys18@gmail.com>
2019-11-03 01:27:30 +0900

    1911

--- 191030/SWEA5251.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdin = open('5251.txt', 'r')
-
-for T in range(int(input())):
-    N, E = map(int,input().split())
-    for i in range(E):
-        s, e, w = map(int,input().split())
-         
