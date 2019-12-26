c4f2347d5a9a73a0dafc75a8c45417fdfb575719
Oizys18 <oizys18@gmail.com>
2019-12-27 00:38:17 +0900

    Mod range

--- 1912/BJ3190.py
+++ 1912/BJ3190.py
@@ -7,13 +7,15 @@
 2. 이동칸에 사과가 있다면, 사과가 없어지고 꼬리는 움직이지 않음
 3. 사과가 없다면 몸길이를 줄여서 꼬리칸을 비워준다. 
 """
+from pprint import pprint as pp 
 
 N = int(input())
 K = int(input())
 mat = [[0]*N for _ in range(N)]
+apple = []
 for k in range(K):
     x, y = map(int, input().split())
-    mat[x-1][y-1] = 2
+    apple.append((x-1, y-1))
 mat[0][0] = 1
 L = int(input())
 rot = {}
@@ -21,13 +23,47 @@ for l in range(L):
     X, C = input().split()
     X = int(X)
     rot[X] = C
+#           오  아래  왼쪽    위
+rotat = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-def solve(x, y):
+def isMap(x, y):
+    if 0 <= x < N and 0 <= y < N:
+        if mat[x][y] == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def solve(x, y, d, t):
     queue = []
-    queue.append((x, y))
-    while queue:
-        x, y = queue.pop(0)
-        if not visit[x][y]:
-            visit[x][y] = 1
-            
+    while True:
+        if t in rot.keys():
+            if rot[t] == 'D':
+                d += 1
+                d = d % 4 
+            else:
+                d -= 1
+                if d == -1:
+                    d = 3
+        nx = x + rotat[d][0]
+        ny = y + rotat[d][1]
+        
+        if isMap(nx, ny):
+            t += 1
+            queue.append((x, y))
+            mat[nx][ny] = 1
+            if (nx, ny) not in apple:
+                a,b = queue.pop(0)
+                mat[a][b] = 0
+            else:
+                apple.remove((nx,ny))
+            x = nx
+            y = ny
+        else:
+            return t+1
+        # pp(mat)
+
+res = solve(0, 0, 0, 0)
+print(res)
