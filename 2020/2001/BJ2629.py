fe048894c49a5a11e71d45c6b9109356cbc84767
Oizys18 <oizys18@gmail.com>
2020-01-11 17:59:28 +0900

    Add one algo

---
+++ 2020/2001/BJ2629.py
@@ -0,0 +1,61 @@
+# 양팔저울
+N = int(input())
+dp = [[0]*15001 for _ in range(31)]
+# weight = [0]*31
+check = [-1, 0, 1]
+
+
+weight = [*map(int, input().split())]
+
+M = int(input())
+marble = [*map(int, input().split())]
+
+def solve():
+    dp[0][0] = 1
+    for i in range(N):
+        for j in range(3):
+            for k in range(15001):
+                nextW = weight[i] * check[j] + k
+                dp[i][abs(nextW)] = dp[i-1][k]
+
+solve()
+# print(dp[N][4])
+
+# for m in range(M):
+#     if dp[N][marble[m]+15000]:
+#         print('Y')
+#     else:
+#         print('N')
+
+
+# 양팔저울 retry
+
+# N = int(input())
+# weights = [*map(int, input().split())]
+# M = int(input())
+# marbles = [*map(int, input().split())]
+# visit = [[0]*15001 for _ in range(31)]
+
+
+# def solve(n, w):
+#     if n > N:
+#         return
+#     if visit[n][w] == 1:
+#         return
+#     visit[n][w] = 1
+#     for i in range(N):
+#         if not visit[n+1][w+weights[i]]:
+#             solve(n+1, w+weights[i])
+#         if not visit[n+1][w]:
+#             solve(n+1, w)
+#         if not visit[n+1][abs(w-weights[i])]:
+#             solve(n+1, abs(w-weights[i]))
+# solve(0, 0)
+
+# for m in marbles:
+#     check = 'N'
+#     for n in range(N):
+#         if visit[n][m]:
+#             check = 'Y'
+#     print(check)
+# print(visit[2][2])
--- 2020/2001/BJ2636.py
+++ 2020/2001/BJ2636.py
@@ -1,49 +1,52 @@
-# # 치즈
-# import collections
-# M, N = map(int, input().split())
-# mat = [[*map(int, input().split())] for _ in range(M)]
-# visit = [[0]*N for _ in range(M)]
+# 치즈
+import collections
+M, N = map(int, input().split())
+mat = [[*map(int, input().split())] for _ in range(M)]
+cheeseNum = 0
 
+def solve():
+    global cheeseNum
+    global mat
 
-# def isCheese(x, y):
-#     if 0 < x < M and 0 < y < N:
-#         if mat[x][y] == 1:
-#             return True
-#         else:
-#             return False
-#     else:
-#         return False
+    def isMap(x, y):
+        if 0 <= x <= M-1 and 0 <= y <= N-1:
+            return True
+        else:
+            return False
 
+    def BFS(x, y):
+        global mat
+        queue = collections.deque()
+        queue.append((x, y))
+        while queue:
+            x, y = queue.popleft()
+            if not visit[x][y]:
+                visit[x][y] = 1
+                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    nx = x + dx
+                    ny = y + dy
+                    if isMap(nx, ny):
+                        if mat[nx][ny] == 1:
+                            mat[nx][ny] = 2
+                        elif mat[nx][ny] == 0:
+                            queue.append((nx, ny))
+    time = 0
+    while True:
+        visit = [[0]*N for _ in range(M)]
+        tempCheese = 0
+        # paint cheese
+        BFS(0, 0)
+        for x in range(M):
+            for y in range(N):
+                if mat[x][y] == 2:
+                    tempCheese += 1
+                    mat[x][y] = 0
+        if tempCheese == 0:
+            return time, cheeseNum
+        else:
+            cheeseNum = tempCheese
+        time += 1
 
-# def solve():
-#     time = 0
-#     cnt = 0
-#     pre = 0
-#     while True:
-#         visit = [[0]*N for _ in range(M)]
-#         for x in range(M):
-#             for y in range(N):
-#                 if not mat[x][y] and not visit[x][y]:
-#                     visit[x][y] = 1
-#                     for dx, dy in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
-#                         nx = x + dx
-#                         ny = y + dy
-#                         if isCheese(nx, ny):
-#                             mat[nx][ny] = 2
-#         flag = 0
-#         for x in range(M):
-#             for y in range(N):
-#                 if mat[x][y] == 2:
-#                     flag = 1
-#                     cnt += 1
-#                     mat[x][y] = 0
-#         if cnt == 5:
-#             print('22')
-#         if not flag:
-#             return time, pre
-#         pre = cnt
-#         cnt = 0
-#         time += 1
-
-
-# print(solve())
+t, c = solve()
+print(t)
+print(c)
