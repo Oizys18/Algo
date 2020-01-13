df00ff76540922b94ebf0dee80c01045f1e458b0
Oizys18 <oizys18@gmail.com>
2020-01-13 19:04:36 +0900

    Add one algo

--- 2020/2001/BJ2629.py
+++ 2020/2001/BJ2629.py
@@ -1,28 +1,55 @@
 # 양팔저울 
 N = int(input())
-weight = [*map(int,input().split())]
+weight = [0]+[*map(int,input().split())]
 M = int(input())
 marble = [*map(int,input().split())]
-
-양팔저울
-N = int(input())
-dp = [[0]*15001 for _ in range(31)]
-# weight = [0]*31
-check = [-1, 0, 1]
-
-
-weight = [*map(int, input().split())]
-
-M = int(input())
-marble = [*map(int, input().split())]
-
+dp = [[0]*15001 for _ in range(N+1)]
+check = [-1,0,1]
 def solve():
-    dp[0][0] = 1
-    for i in range(N):
-        for j in range(3):
+    dp[0][0] = 1    
+    for w in range(1,N+1):
+        for j in check:
             for k in range(15001):
-                nextW = weight[i] * check[j] + k
-                dp[i][abs(nextW)] = dp[i-1][k]
+                nw = weight[w] * j + k 
+                # if nw < 0 or nw > 15000:
+                #     continue
+                # else:
+                dp[w][nw] = dp[w-1][k]
+solve()
+print(dp[2][3])
+
+for m in marble:
+    if dp[N][m]:
+        print('Y')
+    else:
+        print('N')
+
+
+
+
+
+
+# print(dp[2][4])
+# print(dp[])
+
+# # 양팔저울
+# N = int(input())
+# dp = [[0]*15001 for _ in range(31)]
+# # weight = [0]*31
+# check = [-1, 0, 1]
+
+
+# weight = [*map(int, input().split())]
+# M = int(input())
+# marble = [*map(int, input().split())]
+
+# def solve():
+#     dp[0][0] = 1
+#     for i in range(N):
+#         for j in range(3):
+#             for k in range(15001):
+#                 nextW = weight[i] * check[j] + k
+#                 dp[i][abs(nextW)] = dp[i-1][k]
 
 # solve()
 
