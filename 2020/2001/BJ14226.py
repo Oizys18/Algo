7992da952ccf5625b57b4e2e01e41a42da4ec40f
Oizys18 <oizys18@gmail.com>
2020-01-28 19:00:00 +0900

    Add algos

--- 2020/2001/BJ14226.py
+++ 2020/2001/BJ14226.py
@@ -1,24 +1,58 @@
-# 이모티콘 
+# 이모티콘
 S = int(input())
-res = 100
-def solve(k, v, c):
-    global res
-    if v == S:
-        if k < res:
-            res = k
-            print(k)
-        return
-    else:
-        # clipboard
-        if v < S:
-            if v != c:
-                solve(k+1, v, v)
-            # paste 
-            if c != 0:
-                solve(k+1, v+c, c)
-        # delete
-        if v:
-            solve(k+1, v-1, c)
+res = S+1
+visit = [[0]*2000 for _ in range(2001)]
+# BFS가 더 빠르다.. .답 나오자 마자 끝내는 거라.. 
+def BFS(depth, v, c):
+    queue = []
+    queue.append((depth,1,0))
+    while queue:
+        depth, v, c = queue.pop(0)
+        if v == S:
+            return depth
+        if not visit[v][c] or visit[v][c] > depth:
+            visit[v][c] = depth
+            if depth < res:
+                if v != c:
+                    queue.append((depth+1,v,v))
+                if c and v+c < min(2*S,1001):
+                    queue.append((depth+1,v+c, c))
+                if 0 < v <= v+c:
+                    queue.append((depth+1,v-1,c))
+print(BFS(0,1,0))
 
-solve(0, 1, 0)
 
+# def solve(k, v, c):
+#     global res
+#     if v == S:
+#         if k < res:
+#             res = k
+#         return
+#     else:
+#         if not visit[v][c] or visit[v][c] > k:
+#             visit[v][c] = k
+#             if k < res:
+#                 if v != c:
+#                     solve(k+1, v, v)
+#                 if c and v+c < min(2*S,1001):
+#                     solve(k+1, v+c, c)
+#                 if 0 < v <= v+c:
+#                     solve(k+1, v-1, c)
+# solve(0, 1, 0)
+# print(res)
+
+
+"""
+22
+1 0 0
+1 1 1
+2 1 2
+3 1 3
+4 1 4 
+4 4 5 
+8 4 6
+12 4 7
+11 4 8 
+11 11 9
+22 11 10
+"""
--- 2020/2001/BJ17406.py
+++ 2020/2001/BJ17406.py
@@ -1,9 +1,93 @@
-# 배열 돌리기 4 
+from pprint import pprint as pp
+# 배열 돌리기 4
 
-N, M, K = map(int,input().split())
-matA = [[*map(int,input().split())] for _ in range(N)]
-matK = [[*map(int,input().split())] for _ in range(K)]
+N, M, K = map(int, input().split())
+mat = [[0]*(M+1)] + [[0]+[*map(int, input().split())] for _ in range(N)]
+turnK = [[*map(int, input().split())] for _ in range(K)]
+pp(mat)
+
+# 시계방향으로 돌리는 함수
+# 방향 : 우 하 좌 상 우 하 좌 상 우 하 좌 상
+dxdy = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+dr = 0
+
+def turn(r,c,s):
+    global dr
+    L = 2*s + 1
+    turnMat = [[0]*(M+1)] + [[0]+[0]*M for _ in range(N)]
+    x, y = r-s, c-s
+    while True:
+        pp(turnMat)
+        dx,dy = dxdy[dr]
+        nx = x + dx
+        ny = y + dy
+        if r-s <= nx < r+s+1 and r-s <= ny <= r+s+1:
+            print(nx,ny)
+            if turnMat[nx][ny] == 0:
+                turnMat[nx][ny] = mat[x][y]
+                x = nx
+                y = ny
+            else:
+                dr += 1
+                if dr > 3:
+                    break
+                continue
+        else:
+            dr += 1
+            if dr > 3:
+                break
+            continue
+        
+    pp(turnMat)
 
 
 
+for k in turnK:
+    r, c, s = k
+    dr = 0
+    # print(mat[r-s][c-s])
+    turn(r,c,s)
 
+
+
+# def turn(r, c, s):
+#     global dr
+#     def isMap(x, y):
+#         if 0 <= x < N and 0 <= y < M:
+#             if turnMat[x][y] == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return False
+
+#     def move(x, y):
+#         global dr
+#         while True:
+#             flag = 0
+#             dx, dy = dxdy[dr]
+#             nx, ny = x + dx, y + dy
+#             if isMap(nx, ny):
+#                 turnMat[nx][ny] = mat[x][y]
+#                 x = nx
+#                 y = ny
+#             else:
+#                 dr += 1
+#                 if dr > 3:
+#                     dr = 0
+#                     break
+
+
+#     L = 2*s + 1
+#     turnMat = [[0]*M for _ in range(N)]
+#     for i in range(s+1):
+#         print('##')
+#         dr = 0
+#         move(r-s+i-1, c-s+i-1)
+#         pp(turnMat)
+#     # pp(turnMat)
+
+
+# for k in turnK:
+#     r, c, s = k
+#     turn(r, c, s)
