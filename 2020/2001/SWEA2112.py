b800011f6ed7085118da9d3c5c4cab264df9c366
Oizys18 <oizys18@gmail.com>
2020-02-09 23:55:00 +0900

    Mod algo

--- 2020/2001/SWEA2112.py
+++ 2020/2001/SWEA2112.py
@@ -1,55 +1,164 @@
-# 단면의 모든 세로방향에 대해서 동일한 특성의 셀들이 
+# 단면의 모든 세로방향에 대해서 동일한 특성의 셀들이
 # K개 이상 연속적으로 있는 경우에만 성능검사를 통과하게 된다.
 
-from pprint import pprint as pp 
-import itertools 
+from pprint import pprint as pp
 
-# # 함수
-# def check(y):
-#     cnt = 1
-#     x = 0
-#     medi = mat[0][y]
-#     while True:
-#         if cnt == 3:
-#             return True
-#         x += 1
-#         if x < D:
-#             if mat[x][y] == medi:
-#                 cnt += 1 
-#             else:
-#                 medi = mat[x][y]
-#                 cnt = 1
-#         else:
-#             return False
+# 함수
 
-# T = int(input())
-# for testcase in range(1):
-#     D,W,K = map(int,input().split())
-#     mat = [[*map(int,input().split())] for _ in range(D)]
-#     # rotMat = [*zip(*mat)]
-#     checkRes = 0
-#     for y in range(W):
-#         checkRes += check(y)
-#     if checkRes != W:
-#         # 최대 K 만큼 약을 넣는다. 
-#         for i in range(1,K+1):
-#             for line in itertools.combinations(range(1,W+1),i):
-#                 # line 행에 i 만큼 약을 부어본다. 
-#                 for j in range(1,i+1):
-# 
+import copy
+def check(y, K, temp):
+    cnt = 1
+    x = 0
+    medi = temp[0][y]
+    while True:
+        if cnt == K:
+            return True
+        x += 1
+        if x < D:
+            if temp[x][y] == medi:
+                cnt += 1
+            else:
+                medi = temp[x][y]
+                cnt = 1
+        else:
+            return False
 
-for i in itertools.permutations(range(3),3):
-    print(i)
-        
+
+def solve(depth, mat, cnt):
+    global res
+    if cnt >= res:
+        return
+    if depth >= D:
+        return    
+    else:
+        checkCNT = 0
+        for i in range(W):
+            checkCNT += check(i, K, mat)
+
+        if checkCNT != W:
+            # 색칠 안함
+            solve(depth+1, mat, cnt)
+
+            temp = copy.deepcopy(mat)
+
+            # 0으로 칠함
+            temp[depth] = [0]*W
+            solve(depth+1, temp, cnt+1)
+            # 1로 칠함
+            temp[depth] = [1]*W
+            solve(depth+1, temp, cnt+1)
+        else:
+            if res > cnt:
+                res = cnt
+                return
+
+
+T = int(input())
+for testcase in range(T):
+    D, W, K = map(int, input().split())
+    mat = [[*map(int, input().split())] for _ in range(D)]
+    paint = [0]*(D+1)
+    res = K
+    solve(0,mat,0)
+
+    print(f"#{testcase+1} {res}")
+
 
 
 """
 1         
 6 8 3         
+1 1 1 1 0 0 1 0
+0 0 1 1 0 1 0 1
+1 1 1 1 0 0 1 0
+1 1 1 0 0 1 1 0
+1 1 0 1 1 1 1 0
+1 1 1 0 0 1 1 0
+"""
+
+"""
+10
+6 8 3
 0 0 1 0 1 0 0 1
 0 1 0 0 0 1 1 1
 0 1 1 1 0 0 0 0
 1 1 1 1 0 0 0 1
 0 1 1 0 1 0 0 1
 1 0 1 0 1 1 0 1
+6 8 3
+1 1 1 1 0 0 1 0
+0 0 1 1 0 1 0 1
+1 1 1 1 0 0 1 0
+1 1 1 0 0 1 1 0
+1 1 0 1 1 1 1 0
+1 1 1 0 0 1 1 0
+6 8 4
+1 1 0 0 0 1 1 0
+1 0 1 0 0 1 1 1
+0 1 0 0 1 1 0 0
+1 0 1 0 0 0 0 0
+1 1 0 0 0 0 0 0
+1 0 0 0 1 1 1 1
+6 4 4
+1 1 0 0
+0 1 0 1
+0 0 0 1
+1 1 1 1
+1 1 0 1
+1 0 1 0
+6 10 3
+0 1 0 0 0 1 0 0 1 1
+0 1 1 0 0 1 0 0 1 0
+0 1 0 0 1 0 1 1 1 1
+0 0 0 0 0 1 1 1 1 0
+0 1 0 0 1 1 1 1 1 1
+1 0 0 0 1 1 0 0 1 1
+6 6 5
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+6 6 4
+1 1 1 1 1 1
+0 0 0 0 0 1
+0 1 1 1 0 1
+0 1 0 1 0 1
+0 1 0 0 0 1
+0 1 1 1 1 1
+8 15 3
+0 1 1 0 0 1 1 0 1 1 0 0 0 0 0
+1 0 0 0 1 1 0 0 0 0 0 1 0 1 1
+1 1 0 1 0 1 0 1 0 1 0 1 0 0 0
+0 1 1 1 0 0 1 0 0 0 0 1 0 0 0
+0 0 0 0 0 0 1 0 0 0 1 1 0 0 1
+1 0 1 0 0 1 0 1 1 1 1 0 1 1 1
+0 0 0 0 0 1 1 1 0 0 0 0 0 1 0
+0 0 1 0 1 1 0 1 1 0 0 0 1 0 0
+10 20 4
+1 0 1 1 1 1 1 1 1 1 0 0 1 1 1 0 1 1 0 1
+1 1 0 1 1 1 0 0 1 0 0 0 1 1 1 1 0 0 1 0
+1 1 0 1 1 0 0 0 1 1 1 1 1 0 0 1 1 0 1 0
+0 0 0 1 1 0 0 0 0 1 0 0 1 0 1 1 1 0 1 0
+0 1 1 0 1 0 1 0 1 0 0 1 0 0 0 0 1 1 1 1
+1 0 1 0 1 0 1 1 0 0 0 0 1 1 1 0 0 0 0 0
+0 1 0 0 1 1 0 0 0 0 0 1 1 0 0 1 1 0 1 1
+1 0 0 0 0 1 0 1 1 0 1 1 0 1 0 0 1 1 1 0
+0 1 1 0 0 1 0 1 0 0 0 0 0 0 0 1 1 1 0 1
+0 0 0 0 0 0 1 1 0 0 1 1 0 0 0 0 0 0 1 0
+13 20 5
+1 1 0 1 0 0 0 1 1 1 1 0 0 0 1 1 1 0 0 0
+1 1 1 1 0 1 0 1 0 0 0 0 1 0 0 0 0 1 0 0
+1 0 1 0 1 1 0 1 0 1 1 0 0 0 0 1 1 0 1 0
+0 0 1 1 0 1 1 0 1 0 0 1 1 0 0 0 1 1 1 1
+0 0 1 0 0 1 0 0 1 0 0 0 0 1 0 0 0 0 1 1
+0 0 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 1 0 1
+0 0 0 1 0 0 0 0 0 0 1 1 0 0 0 1 0 0 1 0
+1 1 1 0 0 0 1 0 0 1 1 1 0 1 0 1 0 0 1 1
+0 1 1 1 1 0 0 0 1 1 0 1 0 0 0 0 1 0 0 1
+0 0 0 0 1 0 1 0 0 0 1 0 0 0 0 1 1 1 1 1
+0 1 0 0 1 1 0 0 1 0 0 0 0 1 0 1 0 0 1 0
+0 0 1 1 0 0 1 0 0 0 1 0 1 1 0 1 1 1 0 0
+0 0 0 1 0 0 1 0 0 0 1 0 1 1 0 0 1 0 1 0
 """
