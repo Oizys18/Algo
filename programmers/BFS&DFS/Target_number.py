2e3157d419470d4ed0377697d17193feeb5d4185
Oizys18 <oizys18@gmail.com>
2020-08-11 23:47:23 +0900

    Add function development

---
+++ programmers/BFS&DFS/Target_number.py
@@ -0,0 +1,16 @@
+numbers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ]
+target = 10
+
+
+# 문제풀이
+
+def solution(numbers, target):
+    answer = 0
+    N = len(numbers)
+    visit = [0]*N
+    arr = []
+
+    return answer
+
+
+print(solution(numbers, target))
