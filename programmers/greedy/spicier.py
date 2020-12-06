028d8d59ba5d67581d0e77abe138321a4b358766
Oizys18 <oizys18@gmail.com>
2020-12-06 17:43:59 +0900

    Add spice

--- programmers/greedy/spicier.py
+++ programmers/greedy/spicier.py
@@ -3,5 +3,7 @@ scoville =[1, 2, 3, 9, 10, 12]
 K = 7
 def solution(scoville, K):
     answer = 0
-    
+    hotdict = dict()
+    for idx, hot in enumerate(scoville):
+        if 
     return answer
