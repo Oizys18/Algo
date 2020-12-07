7ab8a7632d14ad2fd47cd98ed0a4ed907af59c9d
Oizys18 <oizys18@gmail.com>
2020-12-07 23:40:08 +0900

    Add spicier

--- programmers/greedy/spicier.py
+++ programmers/greedy/spicier.py
@@ -4,6 +4,31 @@ K = 7
 def solution(scoville, K):
     answer = 0
     hotdict = dict()
-    for idx, hot in enumerate(scoville):
-        if 
+#     for idx, hot in enumerate(scoville):
+#         if 
     return answer
+
+
+# 1 -> v 
+# 2   2 -> rr / vv 
+
+# 3  3 -> vvv / 1 
+#        vrr / rrv  2
+ 
+# 5  4 -> vvvv / 1
+#        vvrr / rrvv / vrrv 3
+       # rrrr / 1  
+# 8   5 -> vvvvv / 1
+#        vvvrr / vvrrv / vrrvv / rrvvv / 4
+#        vrrrr / rrvrr / rrrrv /  3 
+# 13  6 -> vvvvvv / 1 
+#       rrvvvv / vrrvvv / vvrrvv / vvvrrv / vvvvrr/   5 
+#       rrrrvv /vrrrrv/ vvrrrr / rrvrrv / rrvvrr / vrrvrr/   6 
+#       rrrrrr / 1
+#  21   7 -> vvvvvvv / 1
+#         vvvvvrr / vvvvrrv/ vvvrrvv/ vvrrvvv/ vrrvvvv/ rrvvvvv/  6
+#         vvvrrrr/ vvrrvrr / vrrvvrr/ rrvvvrr / rrvvrrv / rrvrrvv / vrrvrrv / vvrrrrv / vrrrrvv/ rrrrvvv/  10
+#         vrrrrrr / rrvrrrr/rrrrvrr/ rrrrrrv / 4
+#         
+
+#  n -> n-1 + n-2 
