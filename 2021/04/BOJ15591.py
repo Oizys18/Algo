4df1c48b9ca4fa859942bad4df2272b0d11dec45
Oizys18 <oizys18@gmail.com>
2021-04-16 03:56:34 +0900

    Add BOJ15591

--- 2021/04/BOJ15591.py
+++ 2021/04/BOJ15591.py
@@ -3,49 +3,30 @@ import sys
 sys.stdin = open('BOJ15591.txt', 'r')
 from pprint import pprint as pp 
 
+
+from collections import deque, defaultdict
+
 N,Q = map(int, input().split(' '))
-mat = [[0]*(N+1) for _ in range(N+1)]
-graph = {i:[] for i in range(N+1)}
+graph = defaultdict(list)
 
-for _ in range(N-1):
-    p,q,r = map(int,input().split(' '))
-    mat[p][q] = r
-    mat[q][p] = r
-    graph[p].append(q)
-    graph[q].append(p)
-print(graph)
+for p,q,r in [map(int,input().split(' ')) for _ in range(N-1)]:
+    graph[p].append((q,r))
+    graph[q].append((p,r))
 
-def checkU(start,now,end,usado=[],visit=[0]*(N+1),cnt=0):
-    visit[now] = 1
-    print(now,cnt,visit)
-    if now == end:
-        mat[start][end] = min(usado)
-        mat[end][start] = min(usado)
-        print(min(usado))
-        return
-    nxt_path = graph[now]
-    for edge in nxt_path:
-        if not visit[edge]:
-            visit[edge] = 1
-            usado.append(mat[now][edge])
-            checkU(start,edge,end,usado,visit,cnt+1)
-            usado.pop()
-            visit[edge] = 0
+def solve(k,v,visit):
+    cnt = 0
+    queue = deque()
+    queue.appendleft(v)
+    while queue:
+        edge = queue.popleft()
+        visit[edge] = 1
+        for edge,length in graph[edge]:
+            if length >= k and not visit[edge]:
+                visit[edge] = 1
+                cnt += 1
+                queue.appendleft(edge)
+    return cnt
 
-for e in range(1,N+1):
-    for f in range(e,N+1):
-        if e!=f: 
-            print('---------------',e,f)
-            checkU(e,e,f)
-            pp(mat)
-checkU(3,3,4)
-pp(mat)
-
-# for _ in range(Q):
-#     k,v = map(int,input().split(' '))
-#     temp_usado = []
-#     for e in range(1,N+1):
-#         if e == v:
-#             continue
-#         checkU(v,v,e)
+for k,v in [map(int,input().split(' ')) for _ in range(Q)]:
+    print(solve(k,v,[0]*(N+1)))
 
