8df4edc28196560ae9a2e574ec89f72c478fef9b
Oizys18 <oizys18@gmail.com>
2021-04-29 23:53:39 +0900

    Mod boj14466

--- BOJ/BOJ14466.py
+++ BOJ/BOJ14466.py
@@ -2,6 +2,8 @@
 
 import sys 
 sys.stdin = open('BOJ14466.txt','r')
+from pprint import pprint as pp 
+
 
 N, K, R = map(int,input().split())
 mat = [[0]*N for _ in range(N)]
@@ -9,5 +11,5 @@ road = [list(map(int,input().split())) for _ in range(R)]
 cow = {i:tuple(map(int,input().split())) for i in range(K)}
 
 print(N,K,R)
-print(road)
-print(cow)
+pp(road)
+pp(cow)
