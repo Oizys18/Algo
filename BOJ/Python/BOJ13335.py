b1f4130b411ecf0d68bc8cfb63c2ddfc5705193d
Oizys18 <oizys18@gmail.com>
2021-05-19 23:54:39 +0900

    Add truck

--- BOJ/Python/BOJ13335.py
+++ BOJ/Python/BOJ13335.py
@@ -3,3 +3,13 @@ import sys
 from pprint import pprint as pp 
 sys.stdin = open('BOJ13335.txt', 'r')
 
+
+n,w,L = map(int,input().split())
+que = [*map(int,input().split())]
+bridge = []
+total_w = 0
+time = 0 
+while que:
+    if total_w < L and len(bridge) < w:
+        pass
+    tiem += 1 
