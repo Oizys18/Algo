d739f55fc472ae80880b25fb12b11698168ed420
Oizys18 <oizys18@gmail.com>
2021-05-21 23:58:08 +0900

    add 18428

--- BOJ/Python/BOJ18428.py
+++ BOJ/Python/BOJ18428.py
@@ -2,11 +2,20 @@
 import sys
 from pprint import pprint as pp 
 sys.stdin = open('BOJ18428.txt', 'r')
-
+# 장애물 3개 설치해서 모든 학생 감시 피할 수 있는지 여부 출력 ( YES  or NO)
 N = int(input())
 
 mat = [[*input().split()] for _ in range(N)]
+# turned = list(map(list,zip(*mat)))
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
 pp(mat)
-
-pp(list(map(list,zip(*mat))))
+students = set()
+teachers = set()
+for x in range(N):
+    for y in range(N):
+        if mat[x][y] == 'S':
+            students.add((x,y))
+        elif mat[x][y] == 'T':
+            teachers.add((x,y))
