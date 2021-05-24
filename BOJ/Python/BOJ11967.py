a4280c3222e13baa5db32db212a1994e521807b0
Oizys18 <oizys18@gmail.com>
2021-05-25 00:50:57 +0900

    Add 11967

--- BOJ/Python/BOJ11967.py
+++ BOJ/Python/BOJ11967.py
@@ -17,51 +17,36 @@ for x,y,a,b in signals:
     if not signal_dict.get((x,y)):
         signal_dict[(x,y)] = []
     signal_dict[(x,y)].append((a,b))
-    
+
+pp(signal_dict)
+def lightsOn(x,y,room):
+    cnt = 0
+    if signal_dict.get((x,y)):
+        for nx,ny in signal_dict[(x,y)]:
+            if not room[nx][ny]:
+                room[nx][ny] = 1 
+                cnt += 1 
+    return cnt, room 
 
 def isMap(x,y):
     return 0<x<N+1 and 0<y<N+1
 
-def check(x,y):
+def DFS(x,y,room):   
+    visit = [[0]*(N+1) for _ in range(N+1)]
+    
     q = deque()
     q.append((x,y))
-    visit = [[0]*(N+1) for _ in range(N+1)]
     visit[x][y] = 1 
-    cnt = 0 
+    cnt = 0
     while q:
         x,y = q.popleft()
-        if signal_dict.get((x,y)):
-            for nx,ny in signal_dict[(x,y)]:
-                if not visit[nx][ny]:
-                    visit[nx][ny] = 1
-                    cnt += 1
-
+        this_cnt,room = lightsOn(x,y,room)
+        cnt += this_cnt
         for i in range(4):
             nx,ny = x + dx[i] , y+dy[i]
-            if isMap(nx,ny) and visit[nx][ny]: 
+            if isMap(nx,ny) and room[nx][ny] and not visit[nx][ny]:
+                visit[nx][ny] = 1 
                 q.append((nx,ny))
+    return cnt 
 
-    return cnt
-print(check(1,1))
-
-
-
-
-# def DFS(x,y,mat):   
-#     q = deque()
-#     q.append((x,y))
-#     check_visit = [[0]*(N+1) for _ in range(N+1)]
-#     check_visit[x][y] = 1 
-#     cnt = 0
-#     while q:
-#         x,y = q.popleft()
-#         cnt += 1 
-#         for i in range(4):
-#             nx,ny = x + dx[i] , y+dy[i]
-#             if isMap(nx,ny) and mat[nx][ny] and not check_visit[nx][ny]:
-#                 check_visit[nx][ny] = 1 
-#                 q.append((nx,ny))
-#     return cnt 
-
-# print(DFS(1,1,visit))
-
+print(DFS(1,1,rooms))
