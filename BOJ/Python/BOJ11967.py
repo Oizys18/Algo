4c54826a3118ff1cbeb7c986315cf46309962eac
Oizys18 <oizys18@gmail.com>
2021-05-25 01:10:51 +0900

    Add 11967

--- BOJ/Python/BOJ11967.py
+++ BOJ/Python/BOJ11967.py
@@ -6,9 +6,9 @@ sys.stdin = open('BOJ11967.txt', 'r')
 from collections import deque
 
 N,M = map(int,input().split())
-rooms = [[0]*(N+1) for _ in range(N+1)]
 signals = [[*map(int,input().split())] for _ in range(M)]
-rooms[1][1] = 1 
+lighted = set()
+lighted.add((1,1))
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 
@@ -18,35 +18,37 @@ for x,y,a,b in signals:
         signal_dict[(x,y)] = []
     signal_dict[(x,y)].append((a,b))
 
-pp(signal_dict)
-def lightsOn(x,y,room):
-    cnt = 0
+
+def lightsOn(x,y):
     if signal_dict.get((x,y)):
         for nx,ny in signal_dict[(x,y)]:
-            if not room[nx][ny]:
-                room[nx][ny] = 1 
-                cnt += 1 
-    return cnt, room 
+            lighted.add((nx,ny))
+    return 
 
 def isMap(x,y):
     return 0<x<N+1 and 0<y<N+1
 
-def DFS(x,y,room):   
+def DFS(x,y):   
     visit = [[0]*(N+1) for _ in range(N+1)]
-    
+    visit[x][y] = 1 
     q = deque()
     q.append((x,y))
-    visit[x][y] = 1 
-    cnt = 0
+    cnt = 0 
     while q:
         x,y = q.popleft()
-        this_cnt,room = lightsOn(x,y,room)
-        cnt += this_cnt
+        lightsOn(x,y)
         for i in range(4):
             nx,ny = x + dx[i] , y+dy[i]
-            if isMap(nx,ny) and room[nx][ny] and not visit[nx][ny]:
+            if isMap(nx,ny) and (nx,ny) in lighted and not visit[nx][ny]:
                 visit[nx][ny] = 1 
                 q.append((nx,ny))
-    return cnt 
+    return  
 
-print(DFS(1,1,rooms))
+while True:
+    this = len(lighted)
+    DFS(1,1)
+    if this == len(lighted):
+        break 
+
+print(len(lighted))
+
