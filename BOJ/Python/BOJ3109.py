22240d1311d89c4c1396634e7f1402881ecb4070
Oizys18 <oizys18@gmail.com>
2021-06-08 23:16:11 +0900

    Add 3109

---
+++ BOJ/Python/BOJ3109.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin=open('BOJ3109.txt','r')
+
+from pprint import pprint as pp
+
+
+from collections import deque
+R,C = map(int,input().split())
+mat =[input() for _ in range(R)]
+pipes = [(-1,1),(0,1),(1,1)]
+
+pp(mat)
+
+def isEmpty(x,y):
+    return 0 <= x < R and 0 <= y < C and mat[x][y]=='.'
+
+def solve(x,y):
+    if y == C-1:
+        print('도착!')
+        pp(visit)
+        return 
+    else:
+        for i in range(3):
+            nx,ny = x+pipes[i][0], y+pipes[i][1]
+            print(nx,ny)
+            if isEmpty(nx,ny) and not visit[nx][ny]:
+                visit[nx][ny] = 1 
+                return solve(nx,ny)
+                visit[nx][ny] = 0
+
+visit =[[0]*C for _ in range(R)]
+for i in range(C):
+    print(0,i,'----------')
+    solve(0,i)
+    
