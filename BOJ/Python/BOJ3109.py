a34841550adc63247d65f15493fc12c753612191
Oizys18 <oizys18@gmail.com>
2021-06-11 23:47:47 +0900

    add 3109

--- BOJ/Python/BOJ3109.py
+++ BOJ/Python/BOJ3109.py
@@ -3,35 +3,39 @@ sys.stdin=open('BOJ3109.txt','r')
 
 from pprint import pprint as pp
 
-
-from collections import deque
 R,C = map(int,input().split())
 mat =[input() for _ in range(R)]
 pipes = [(-1,1),(0,1),(1,1)]
 
-pp(mat)
 
 def isEmpty(x,y):
     return 0 <= x < R and 0 <= y < C and mat[x][y]=='.'
 
-def solve(x,y):
+def solve(x,y,visited):
     if y == C-1:
-        print('도착!')
-        pp(visit)
-        return 
+
+        return 'arrived'
     else:
         for i in range(3):
             nx,ny = x+pipes[i][0], y+pipes[i][1]
-            print(nx,ny)
             if isEmpty(nx,ny) and not visit[nx][ny]:
                 visit[nx][ny] = 1 
-                return solve(nx,ny)
+                visited.add((nx,ny))
+                return solve(nx,ny,visited)
+                visited.remove((nx,ny))
                 visit[nx][ny] = 0
+    return visited
 
 visit =[[0]*C for _ in range(R)]
-for i in range(C):
-    solve(0,i)
-    
-"""
-직전 탐색 데이터 저장 방법 필요 
-"""
+answer = 0
+for i in range(R):
+    tried = solve(i,0,set())
+    if tried=='arrived':
+        answer += 1
+    else:
+        for x,y in tried:
+            visit[x][y]=0
+    pp(visit)
+print(answer)
+
+
