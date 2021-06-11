92f389f66a2569dd63e3c0abd6c6cdfe754756f9
Oizys18 <oizys18@gmail.com>
2021-06-12 00:03:04 +0900

    Add 3109

--- BOJ/Python/BOJ3109.py
+++ BOJ/Python/BOJ3109.py
@@ -11,31 +11,24 @@ pipes = [(-1,1),(0,1),(1,1)]
 def isEmpty(x,y):
     return 0 <= x < R and 0 <= y < C and mat[x][y]=='.'
 
-def solve(x,y,visited):
+def solve(x,y):
     if y == C-1:
-
-        return 'arrived'
+        return True
     else:
         for i in range(3):
             nx,ny = x+pipes[i][0], y+pipes[i][1]
             if isEmpty(nx,ny) and not visit[nx][ny]:
                 visit[nx][ny] = 1 
-                visited.add((nx,ny))
-                return solve(nx,ny,visited)
-                visited.remove((nx,ny))
-                visit[nx][ny] = 0
-    return visited
+                if solve(nx,ny):
+                    return True
+                
+    return False
 
 visit =[[0]*C for _ in range(R)]
 answer = 0
 for i in range(R):
-    tried = solve(i,0,set())
-    if tried=='arrived':
+    if solve(i,0):
         answer += 1
-    else:
-        for x,y in tried:
-            visit[x][y]=0
-    pp(visit)
 print(answer)
 
 
