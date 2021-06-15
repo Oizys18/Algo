8925a9d2e0814135cc2d2c86a799f6a1bc14c503
Oizys18 <oizys18@gmail.com>
2021-06-15 17:28:15 +0900

    Add 1027

--- BOJ/Python/BOJ1027.py
+++ BOJ/Python/BOJ1027.py
@@ -2,3 +2,20 @@ import sys
 from pprint import pprint as pp 
 sys.stdin = open('BOJ1027.txt', 'r')
 
+N = int(input())
+buildings = [*map(int,input().split())]
+
+print(N)
+print(buildings)
+
+def solve(N,buildings):
+    for i in range(N):
+        top = 0
+        for j in range(i+1,N):
+            # 가시 확인 
+            
+            # 탑 재설정 
+            top = max(buildings[j],top)
+            
+
+solve(N,buildings)
