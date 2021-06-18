8514f2519f3ae009deeed6f070c234238847a4db
Oizys18 <oizys18@gmail.com>
2021-06-18 19:58:31 +0900

    Add BOJ1655

--- BOJ/Python/BOJ10655.py
+++ BOJ/Python/BOJ10655.py
@@ -5,47 +5,15 @@ sys.stdin = open('BOJ10655.txt', 'r')
 
 N = int(input())
 mat = [[*map(int,input().split())] for _ in range(N)]
-shortest_length = 100000 * 4000
-dp = [[0]*N for _ in range(N)]
 
-for skip in range(0,2):
-    pre = 0
-    for now in range(1,N):
-        print(pre,now+skip)
-        if now+skip > N-1:
-            break
-        x1,y1 = mat[pre]
-        x2,y2 = mat[now+skip]
-        if not dp[pre][now+skip]:
-            dp[pre][now+skip]= abs(x1-x2) + abs(y1-y2)
-        pre = now 
+def getLength(p1,p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
 
-# for skip in range(1,N-1):
-#     length = 0
-#     pre = 0 
-#     for now in range(1,N):
-#         if now == skip:
-#             continue 
-#         else:
-#             print(pre,now)
-#             x1,y1 = mat[pre]
-#             x2,y2 = mat[now]
-#             if not dp[pre][now]:
-#                 dp[pre][now]= abs(x1-x2) + abs(y1-y2)
-#             pre = now 
-# pp(dp)
+skipped = 0
+total = getLength(mat[0],mat[1]) 
+for mid in range(1,N-1):
+    gap = getLength(mat[mid-1],mat[mid]) + getLength(mat[mid],mat[mid+1]) - getLength(mat[mid-1],mat[mid+1])
+    skipped = max(skipped, gap)
+    total += getLength(mat[mid],mat[mid+1])
 
-for skip in range(1,N-1):
-    length = 0
-    pre = 0
-    for now in range(1,N):
-        if now == skip:
-            continue 
-        else:
-            print(pre,now)
-            x1,y1 = mat[pre]
-            x2,y2 = mat[now]
-            length += abs(x1-x2) + abs(y1-y2)
-            pre = now 
-    shortest_length = min(length, shortest_length)
-print(shortest_length)
+print(total-skipped)
