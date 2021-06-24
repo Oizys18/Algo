b473635e097df0618a35f857a0327fe1b7158ec6
Oizys18 <oizys18@gmail.com>
2021-06-24 23:59:03 +0900

    Add 2580

--- BOJ/Python/BOJ2580.py
+++ BOJ/Python/BOJ2580.py
@@ -2,3 +2,22 @@ import sys
 sys.stdin = open('BOJ2580.txt','r')
 from pprint import pprint as pp
 #스도쿠
+mat = [[*map(int,input().split())] for _ in range(9)]
+
+
+for x in range(9):
+    check = [0]*10
+    empty = 0
+    print(check)
+    for y in range(9):
+        print(mat[x][y])
+        check[mat[x][y]] = 1 
+        if not mat[x][y]:
+            empty = (x,y)
+    if sum(check) == 9:
+        pp(mat)
+        print(check)
+        for c in range(9):
+            if not check[c]:
+                mat[empty[0]][empty[1]] = c
+        pp(mat)
