1d8694849e495517229bc2224915c1b7a75609de
Oizys18 <oizys18@gmail.com>
2021-06-30 20:42:06 +0900

    Add 17779

--- BOJ/Python/BOJ17779.py
+++ BOJ/Python/BOJ17779.py
@@ -4,61 +4,101 @@ from pprint import pprint as pp
 # 게리맨더링2
 N = int(input())
 mat =[[*map(int,input().split())] for _ in range(N)]
-# dr = [(1,-1),(1,1),(-1,1),(-1,-1)]
+
+def isMap(x,y):
+    return 0<=x<N and 0<=y<N
+def fill(r,c,color):
+    q = []
+    q.append((r,c))
+    check[r][c]=color
+    while q:
+        r,c = q.pop(0)
+        for i in range(4):
+            nr,nc = r+dx[i],c+dy[i]
+            if not check[nr][nc]:
+                q.append((nr,nc))
+                check[nr][nc] = color
+    pp(check)
 
 
-
-
+dx=[0,0,-1,1]
+dy=[-1,1,0,0]
 
 # 기준점 x,y와 d1,d2가 정해지면 선거구가 결정됨 
-check = [[0]*N for _ in range(N)]
-for x in range(N-2):
+ans = 2000
+for x in range(N-1):
     for y in range(1,N-1):
-        check[x][y] = 1 
-        # 
-pp(check)
-        # if (y == 0 or y==N-1) or x >= N-2:
-        #     continue
-        # for d1 in range(1,min(y+1,N-x-1)):
-        #     # print(min(N-x-d1+1,N-y-1),N-y-1,N-x-d1+1 )
-        #     # for d2 in range(1,min(N-x-d1,N-y-d1+1)):
-        #     for d2 in range(1,min(N-x-d1,N-y-d1+1)):
-        #         # x,y에서 d1 만큼 좌하단, d2만큼 우하단, d1만큼 우상단, d2만큼 좌상단 이동 
-        #         top =(x,y)
-        #         left = (x+d1,y-d1)
-        #         bottom = (x+d1+d2,y-d1+d2)
-        #         right = (x+d2,y+d2)
+        
+        for d1 in range(1,y+1):
+            for d2 in range(1,N-x-d1+1):
+                check = [[0]*N for _ in range(N)]
+                top =(x,y)
+                left = (x+d1,y-d1)
+                bottom = (x+d1+d2,y-d1+d2)
+                right = (x+d2,y+d2)
 
-        #         print(x,y,d1,d2,'-------------------')
-        #         print(top,left,bottom,right,N-x-d1,N-y-d1)
-        #         for r in range(N):
-        #             for c in range(N):
-        #                 if 0 <= r <left[0] and 0<=c<=top[1]:
-        #                     check[r][c] = 1
-        #                 elif 0<= r <= right[0] and top[1] < c <N:
-        #                     check[r][c] = 2
-        #                 elif left[0]<= r <N and 0<= c < bottom[1]:
-        #                     check[r][c] = 3
-        #                 elif right[0]< r < N and bottom[1]<= c < N : 
-        #                     check[r][c] = 4
-        #         # print(check[x][y],check[x+d1][y-d1],check[x+d1+d2][y-d1+d2], check[x+d2][y+d2] )
-        #         check[x][y] = 5
-        #         check[x+d1][y-d1]=5
-        #         check[x+d1+d2][y-d1+d2]=5
-        #         check[x+d2][y+d2] =5
+                if isMap(bottom[0],bottom[1]) and isMap(right[0],right[1]):
+                    one = 0
+                    two = 0
+                    three =0
+                    four = 0
+                    five = 0
+                    for d in range(d1+1):
+                        # five += mat[x+d][y-d]
+                        # five += mat[x+d2+d][y+d2-d]
+                        
+                        check[x+d][y-d] = 5
+                        check[x+d2+d][y+d2-d] = 5
+                        if d == d1:
+                            continue
+                        # nr,nc = x+d+1,y-d
+                        # while isMap(nr,nc) and check[nr][nc] != 5:
+                        #     check[nr][nc] = 5 
+                        #     nr += 1 
 
+                    for dd in range(d2+1):
+                        # five += mat[x+dd][y+dd]
+                        # five += mat[x+d1+dd][y-d1+dd]
+                        check[x+dd][y+dd] =5
+                        check[x+d1+dd][y-d1+dd] = 5
+                        if dd == d2:
+                            continue
+                        # nr,nc = x+dd+1,y+dd
+                        # while isMap(nr,nc) and check[nr][nc] != 5:
+                        #     check[nr][nc] = 5 
+                        #     nr += 1 
+                    
 
-        #         pp(check)
+                    for r in range(N):
+                        for c in range(N):
+                            if not check[r][c]:
+                                if 0 <= r <left[0] and 0<=c<=top[1]:
+                                    check[r][c] = 1
+                                    # one += mat[r][c]
+                                elif 0<= r <= right[0] and top[1] < c <N:
+                                    check[r][c] = 2
+                                    # two += mat[r][c]
+                                elif left[0]<= r <N and 0<= c < bottom[1]:
+                                    check[r][c] = 3
+                                    # three += mat[r][c]
+                                elif right[0]< r < N and bottom[1]<= c < N : 
+                                    check[r][c] = 4
+                                    # four += mat[r][c]
+                    pp(check)
 
+                    for a in range(N):
+                        for b in range(N):
+                            if check[a][b] == 1:
+                                one += mat[a][b]
+                            elif check[a][b] == 2:
+                                two += mat[a][b]
+                            elif check[a][b] == 3:
+                                three += mat[a][b]
+                            elif check[a][b] == 4:
+                                four += mat[a][b]
+                            elif check[a][b] == 5:
+                                five += mat[a][b]
 
-
-# def paint(x,y,d1,d2):
-    
-
-
-
-
-# 기준점 x,y 
-def solve(x,y):
-    # return 넓이차 
-    pass
+                    ans = min(max(one,two,three,four,five) - min(one,two,three,four,five), ans)
+                    print(ans,'------------------')
+print(ans)
