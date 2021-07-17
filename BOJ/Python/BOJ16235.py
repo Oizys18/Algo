8b567acef5123c221260620091f17ff62a1bde2f
Oizys18 <oizys18@gmail.com>
2021-07-17 23:26:17 +0900

    Add inputs

--- BOJ/Python/BOJ16235.py
+++ BOJ/Python/BOJ16235.py
@@ -4,3 +4,5 @@ from pprint import pprint as pp
 
 
 N,M,K = map(int,input().split())
+Amat = [[*map(int,input().split())] for _ in range(N)]
+tree_info = [[*map(int,input().split())] for _ in range(M)]
