880764677f14442e6e3e808944e755d70dbbf81d
Oizys18 <oizys18@gmail.com>
2021-07-18 20:09:44 +0900

    Mod input

--- BOJ/Python/BOJ16235.py
+++ BOJ/Python/BOJ16235.py
@@ -4,5 +4,13 @@ from pprint import pprint as pp
 
 
 N,M,K = map(int,input().split())
-Amat = [[*map(int,input().split())] for _ in range(N)]
+land = [[5]*N for _ in range(N)]
+nutrition = [[*map(int,input().split())] for _ in range(N)]
 tree_info = [[*map(int,input().split())] for _ in range(M)]
+
+def solve():
+    pass
+
+pp(land)
+pp(nutrition)
+pp(tree_info)
