1dabc415af7bad71342912baf3738560085e4467
Oizys18 <oizys18@gmail.com>
2021-07-27 03:47:11 +0900

    Add 16967

--- BOJ/Python/BOJ16967.py
+++ BOJ/Python/BOJ16967.py
@@ -4,7 +4,8 @@ sys.stdin = open('BOJ16967.txt','r')
 from pprint import pprint as pp
 
 """
-크기가 H × W인 배열 A와 두 정수 X와 Y가 있을 때, 크기가 (H + X) × (W + Y)인 배열 B는 배열 A와 배열 A를 아래로 X칸, 오른쪽으로 Y칸 이동시킨 배열을 겹쳐 만들 수 있다. 수가 겹쳐지면 수가 합쳐진다.
+크기가 H × W인 배열 A와 두 정수 X와 Y가 있을 때, 
+크기가 (H + X) × (W + Y)인 배열 B는 배열 A와 배열 A를 아래로 X칸, 오른쪽으로 Y칸 이동시킨 배열을 겹쳐 만들 수 있다. 수가 겹쳐지면 수가 합쳐진다.
 
 즉, 배열 B의 (i, j)에 들어있는 값은 아래 3개 중 하나이다.
 
@@ -15,7 +16,19 @@ from pprint import pprint as pp
 """
 
 H,W,X,Y = map(int,input().split())
-mat = [[*map(int,input().split())] for _ in range(H+X)]
-pp(mat)
-pp(H,W,X,Y)
+matB = [[*map(int,input().split())] for _ in range(H+X)]
+matA = [[0]*W for _ in range(H)]
 
+for x in range(H):
+    for y in range(W):
+        if x >= X and y >= Y:
+            matA[x][y] = matB[x][y] - matA[x-X][y-Y]
+        else:
+            matA[x][y] = matB[x][y]
+            
+for x in range(H):
+    for y in range(W):
+        if y ==W-1:
+            print(matA[x][y])
+        else:
+            print(matA[x][y],end=' ')
