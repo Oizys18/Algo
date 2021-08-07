3c353ba2a9011a7a80baaff298f0ff14cd8d0e08
Oizys18 <oizys18@gmail.com>
2021-08-07 22:57:59 +0900

    Add 9874

--- BOJ/Python/BOJ9874.py
+++ BOJ/Python/BOJ9874.py
@@ -3,11 +3,69 @@ from pprint import pprint as pp
 sys.stdin = open('BOJ9874.txt', 'r')
 
 N = int(input())
-d = dict()
-for _ in range(N):
-    a,b = map(int,input().split())
-    if not d.get(b):
-        d[b] = []
-    d[b].append(a)
-pp(d)    
+holes = [[*map(int,input().split())] for _ in range(N)]
 
+def get_hole(hole):
+    sx,sy = hole
+    nx = 1000000000
+    nxt_i = 0
+    for i in range(N):
+        a,b = holes[i]
+        if sy == b and a > sx:
+            if a < nx:
+                nx = a
+                nxt_i = i
+    if nx == 1000000000:
+        return 0 
+    return nxt_i
+
+def check(paired):
+    for i in range(N):
+        visit = [0]*N
+        visit[i] = 1
+        
+        # 다음 웜홀 구하기 
+        nxt_hole = get_hole(holes[i])
+        while nxt_hole: 
+            for x,y in paired:
+                flag = 0 
+                if nxt_hole == x:
+                    if not visit[y]:
+                        visit[y] = 1 
+                        nxt_hole = get_hole(holes[y])
+                    else:
+                        flag = 1
+                        break 
+                elif nxt_hole == y:
+                    if not visit[x]:
+                        visit[x] = 1 
+                        nxt_hole = get_hole(holes[x])
+                    else:
+                        flag = 1
+                        break 
+            if flag:
+                break        
+        if flag:
+            return True 
+
+answer=[]
+def pair(k,visit,paired):
+    if k == N//2 :
+        if check(paired):
+            answer.append(1)  
+        return 
+    else:
+        for i in range(k,N):
+            if not visit[i]:
+                visit[i] = 1 
+                for j in range(i,N):
+                    if not visit[j]:
+                        visit[j] = 1 
+                        paired.append((i,j))
+                        pair(k+1,visit,paired)
+                        paired.pop()
+                        visit[j] = 0 
+                visit[i] = 0
+pair(0,[0]*N,[])
+
+print(sum(answer))
