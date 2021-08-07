fbb77131da4197595ab5127c82beae7ec1ef0b6f
Oizys18 <oizys18@gmail.com>
2021-08-07 15:47:17 +0900

    Add 9874

--- BOJ/Python/BOJ9874.py
+++ BOJ/Python/BOJ9874.py
@@ -2,3 +2,12 @@ import sys
 from pprint import pprint as pp 
 sys.stdin = open('BOJ9874.txt', 'r')
 
+N = int(input())
+d = dict()
+for _ in range(N):
+    a,b = map(int,input().split())
+    if not d.get(b):
+        d[b] = []
+    d[b].append(a)
+pp(d)    
+
