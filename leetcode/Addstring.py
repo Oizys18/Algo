6c4a27b6eeb290bd8f4e65f0e96266d875fe4c0a
Oizys18 <oizys18@gmail.com>
2021-08-13 23:16:05 +0900

    Add Addstring

---
+++ leetcode/Addstring.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        return str(eval(f'{num1} + {num2}'))
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        a = int(num1)
+        b = int(num2)
+        return str(a+b)
