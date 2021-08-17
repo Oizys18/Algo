5ff7aef1ce3a844f6fa85fd0fd99cc162d4cb1d2
Oizys18 <oizys18@gmail.com>
2021-08-17 22:54:00 +0900

    Add leet algos

---
+++ leetcode/MinimumWindowSubstring.py
@@ -0,0 +1,64 @@
+"""
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+"""
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+# L ------R 
+#     L---R
+#     L-------------R
+#       L-----------R
+        d = dict()
+        for i in t:
+            if not d.get(i):
+                d[i] = 1
+            else:
+                d[i] += 1
+        
+        L = 0
+        R = 0
+
+        for idx,val in enumerate(s):
+            if val in t:
+                d[val] -= 1 
+                if sum(d.values()) == 0:
+                    R = idx 
+                    pass 
+                pass
+
+    def contract():
+        return 
