2e65f9fb34df1328a959a645fbf177e84fbad611
Yang Chan Woo <oizys18@gmail.com>
2021-09-01 23:33:02 +0900

    Add spy desc

--- programmers/prepare_kakao/Spy.py
+++ programmers/prepare_kakao/Spy.py
@@ -25,3 +25,11 @@ def solution(clothes):
     for v in cloth_dict.values():
         answer *= (v+1)
     return answer-1
+
+"""
+nPr n개에서 r개 뽑아 나열(중복가능) == n!/(n-r)!
+nCr n개에서 r개 선택하는 조합 == nPr / r! == n*(n-1)*(n-2)*...*(n-r+1) / r*(r-1)*(r-2)*1
+
+위와 같은 경우 각 카테고리 별 (n+1)C1을 모두 곱해준다음 ==(n개 + 해당 카테고리에서 안고르는 경우)C(1개 고르기)   
+아무것도 위장을 안하면 안된다고 하니 (-1)을 해주면 된다. 
+"""
