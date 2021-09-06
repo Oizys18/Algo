a90d815c428eb051cd6e82dcbe852ca3e4b36084
Yang Chan Woo <oizys18@gmail.com>
2021-09-06 23:37:09 +0900

    Add cctv

--- programmers/prepare_kakao/CCTV.py
+++ programmers/prepare_kakao/CCTV.py
@@ -1,5 +1,11 @@
+routes = [[-20,15], [-14,-5], [-18,-13], [-5,-3]]
+
 def solution(routes):
     answer = 0
     routes.sort()
+    
     print(routes)
+
     return answer
+
+print(solution(routes))
