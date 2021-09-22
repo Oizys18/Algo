562be452f7e2d61ec60b2851be7e0db0e31c1083
Yang Chan Woo <oizys18@gmail.com>
2021-09-22 22:31:58 +0900

    Add triangle

--- programmers/prepare_kakao/Triangle.py
+++ programmers/prepare_kakao/Triangle.py
@@ -1,3 +1,13 @@
+from pprint import pprint as pp
 def solution(triangle):
     answer = 0
+    N = len(triangle)
+    dp = [0]*N
+    pp(triangle)
+    pp(dp)
+    
     return answer
+
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+
+print(solution(triangle))
