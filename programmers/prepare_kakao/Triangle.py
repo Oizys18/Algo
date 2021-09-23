129cf12c76e5cbd1dd5af25a6dfba31ba3215505
Yang Chan Woo <oizys18@gmail.com>
2021-09-23 23:52:28 +0900

    Add dp triangle

--- programmers/prepare_kakao/Triangle.py
+++ programmers/prepare_kakao/Triangle.py
@@ -1,13 +1,20 @@
-from pprint import pprint as pp
 def solution(triangle):
     answer = 0
     N = len(triangle)
-    dp = [0]*N
+    dp = [[0]*i for i in range(1,N+1)]
     for i in range(N):
         for j in range(len(triangle[i])):
-            print(triangle[i][j])
-            
-    return answer
+            if i == 0: dp[i][j] = triangle[i][j]
+            else:
+                if j == 0 :
+                    dp[i][j] = triangle[i][j] + dp[i-1][j]
+                elif j == len(triangle[i])-1:
+                    dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+                else:
+                    l = triangle[i][j] + dp[i-1][j-1]
+                    r = triangle[i][j] + dp[i-1][j]
+                    dp[i][j] = max(dp[i][j],l,r)
+    return max(dp[N-1])
 
 triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 
