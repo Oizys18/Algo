745acf5209bf2d5de88dde5fd9350a9de18c07f4
Yang Chan Woo <oizys18@gmail.com>
2021-09-23 23:38:17 +0900

    Add algo

--- programmers/prepare_kakao/Triangle.py
+++ programmers/prepare_kakao/Triangle.py
@@ -3,9 +3,10 @@ def solution(triangle):
     answer = 0
     N = len(triangle)
     dp = [0]*N
-    pp(triangle)
-    pp(dp)
-    
+    for i in range(N):
+        for j in range(len(triangle[i])):
+            print(triangle[i][j])
+            
     return answer
 
 triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
