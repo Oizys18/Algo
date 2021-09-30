d9da1d4b423e64332f41aeda0478972701fc3393
Yang Chan Woo <oizys18@gmail.com>
2021-09-30 18:38:30 +0900

    Add scoville

---
+++ programmers/prepare_kakao/Scoville.py
@@ -0,0 +1,17 @@
+from heapq import * 
+
+def solution(scoville, K):
+    answer = 0
+    heapify(scoville)
+    while len(scoville)>1:     
+        first = heappop(scoville)
+        if first >= K:
+            break
+            
+        second = heappop(scoville)
+        mixed = first + second*2 
+        heappush(scoville,mixed)
+        answer += 1 
+    if min(scoville) < K:
+        return -1 
+    return answer
