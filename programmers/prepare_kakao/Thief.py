96d1c0faf2d01cf04546f05ca3819aeefc476d8d
Yang Chan Woo <oizys18@gmail.com>
2021-09-30 18:30:13 +0900

    Add thief

--- programmers/prepare_kakao/Thief.py
+++ programmers/prepare_kakao/Thief.py
@@ -1,18 +1,16 @@
-from collections import defaultdict
 def solution(money):
-    answer = 0
     N = len(money)
-    dp = defaultdict(int)
-    bitmask = bin(1 << N)
+    # dp : 0번째 집 안훔침 , dp2: 0번째 집 훔침 
+    dp = [0]*N
+    dp2 = [0]*N
+    # 0번째,1번째는 0번째 집을 훔친 값이 최댓값이다. 
+    dp2[0] = dp2[1] = money[0]
+        
+    # 0번째 집을 안훔쳤기 때문에 N-1까지 탐색 
+    for j in range(1,N):
+        dp[j] = max(dp[j-2]+money[j], dp[j-1])    
+    # 0번째 집을 훔쳤기 때문에 N-2까지 탐색 
+    for i in range(2,N-1):
+        dp2[i] = max(dp2[i-2]+money[i], dp2[i-1])
     
-    # print
-    print(dp)
-    print(bitmask)
-    
-     
-    
-    for i,v in enumerate(money):
-        print(i,v)
-    # 비트마스크로 풀기 
-    
-    return answer
+    return max(dp[N-1],dp2[N-2])
