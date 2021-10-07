7f0a1476ca0e91d066b0ae0e5ef2fd7eee0e0a3c
Yang Chan Woo <oizys18@gmail.com>
2021-10-07 23:59:05 +0900

    Add muzi

--- programmers/prepare_kakao/2020blind/muzimukbang.py
+++ programmers/prepare_kakao/2020blind/muzimukbang.py
@@ -1,3 +1,11 @@
 def solution(food_times, k):
     answer = 0
+    N = len(food_times)
+    if k <= N :
+        print(k)
+    turn,remain = divmod(k,N)
+    print(turn,remain)
+    
+        
+
     return answer
