0f8f17e9169da95dc41feb96c37f9dd12b3de409
Yang Chan Woo <oizys18@gmail.com>
2021-10-13 04:08:17 +0900

    Add tomato explanation

--- BOJ/Python/BOJ7569.py
+++ BOJ/Python/BOJ7569.py
@@ -1,8 +1,10 @@
 import sys 
 sys.stdin = open('BOJ7569.txt','r')
 from pprint import pprint as pp 
-
-from copy import deepcopy as dc 
+"""
+처음 내가 푼 것. deque 넣으니까 풀리긴 했는데 3000ms 정도 걸림 
+"""
+from collections import deque
 M,N,H = map(int,input().split())
 box = [[[0]*H for _ in range(M)] for _ in range(N) ]
 unripe_tomato = set()
@@ -15,67 +17,74 @@ for h in range(H):
                 unripe_tomato.add((n,i,h))
             if m == 1:
                 riped_tomato.add((n,i,h))
-                
+
 def isMap(x,y,z):
     return 0 <= x < N and 0 <= y < M and 0 <= z < H and box[x][y][z] == 0
 
-def isRipe(box):
-    for h in range(H):
-        for n in range(N):
-            for m in range(M):
-                if box[n][m][h] == 0:
-                    return False
-    else:
-        return True 
-
 # x,y,z (n,m,h)
 dr = [(0,1,0),(0,-1,0),(1,0,0),(-1,0,0),(0,0,1),(0,0,-1)]
-visit = [[[0]*H for _ in range(M)] for _ in range(N) ]
 
 def bfs(riped_tomato):
-    q = [(0,x,y,z) for x,y,z in riped_tomato]
+    q = deque((0,x,y,z) for x,y,z in riped_tomato)
     visit = [[[0]*H for _ in range(M)] for _ in range(N)]
     mx_depth = 0
     ripen = set()
     while q:
-        depth,x,y,z = q.pop(0)
+        depth,x,y,z = q.popleft()
         mx_depth = max(depth,mx_depth)
         for dx,dy,dz in dr:
             nx,ny,nz = x+dx,y+dy,z+dz
-            if isMap(nx,ny,nz) and not visit[nx][ny][nz] :
-                visit[nx][ny][nz] = depth+1 
+            if isMap(nx,ny,nz) and not visit[nx][ny][nz]:
+                visit[nx][ny][nz] = 1 
                 q.append((depth+1,nx,ny,nz))
                 ripen.add((nx,ny,nz))
     return mx_depth,ripen
+
 mxD, ripen = bfs(riped_tomato)
+unripe_tomato.difference_update(ripen)
+
+if unripe_tomato:
+    print(-1)
+else:
+    print(mxD)
 
 
-# def ripe(riped):
-#     ripen = set()
-#     for x,y,z in riped:
-#         for dx,dy,dz in dr:
-#             nx,ny,nz = x+dx,y+dy,z+dz
-#             if isMap(nx,ny,nz) and (nx,ny,nz) not in ripen:
-#                 ripen.add((nx,ny,nz))
-#     return ripen
 
+"""
+1등 코드 참고해서 수정함 
+"""
+from collections import deque
+M,N,H = map(int,input().split())
+box = [[[0]*M for _ in range(N)] for _ in range(H) ]
+dr = [(0,1,0),(0,-1,0),(1,0,0),(-1,0,0),(0,0,1),(0,0,-1)]
+q = deque()
+ripe = 0 
+num = 0
+for h in range(H):
+    box[h] = [[*map(int,input().split())] for _ in range(N)]
+    for n in range(N):
+        for m in range(M):
+            if box[h][n][m] == 1:
+                q.append((h,n,m))
+                ripe += 1
+                num += 1 
+            elif box[h][n][m] == -1:
+                ripe += 1
+def isRaw(h,n,m):
+    return 0 <= h < H and 0 <= n < N and 0 <= m < M and box[h][n][m] == 0
 
-# time = 0
-# while True:
-#     if isRipe(box):
-#         print(time)
-#         break
-#     time += 1 
-#     pre_box = dc(box)
-#     riped = set()
-#     for h in range(H):
-#         for n in range(N):
-#             for m in range(M):
-#                 if box[n][m][h] == 1:
-#                     riped.add((n,m,h))
-#     ripen = ripe(riped)
-#     for x,y,z in ripen:
-#         box[x][y][z] = 1 
-#     if box == pre_box:
-#         print(-1)
-#         break
+cnt = 0 
+while q:
+    h,n,m = q.popleft()
+    num -= 1
+    for dh,dn,dm in dr:
+        nh,nn,nm = h+dh,n+dn,m+dm
+        if isRaw(nh,nn,nm):
+            box[nh][nn][nm] = 1
+            ripe += 1 
+            q.append((nh,nn,nm))
+    if num == 0:
+        cnt += 1 
+        num = len(q)
+
+print(cnt-1 if ripe == H*M*N else -1)
