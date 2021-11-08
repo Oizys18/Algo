1f6bf83ea1db36d6208c9365aa3ce54e8e29bbb3
Yang Chan Woo <oizys18@gmail.com>
2021-11-08 23:58:30 +0900

    Add RtI

---
+++ leetcode/RomanToInteger.py
@@ -0,0 +1,15 @@
+""" 
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+Given a roman numeral, convert it to an integer.
+"""
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
