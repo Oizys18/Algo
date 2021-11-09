b3db702cbb0691938633faa9a7c6ec32335af33c
Yang Chan Woo <oizys18@gmail.com>
2021-11-10 00:25:38 +0900

    Add test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-a = [0,1,3,0,4,12,0]
+a = [0, 1, 3, 0, 4, 12, 0]
 N = len(a)
 
 for i in range(N):
-    for j in range(i+1,N):
+    for j in range(i+1, N):
         if a[i] == 0 and a[j]:
-            a[i],a[j]=a[j],a[i]
+            a[i], a[j] = a[j], a[i]
