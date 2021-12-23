82f5565f553055b4475b223021b14dafcc763ba0
Yang Chan Woo <oizys18@gmail.com>
2021-12-23 22:46:50 +0900

    Add lp

--- leetcode/LongestPrefix.py
+++ leetcode/LongestPrefix.py
@@ -6,3 +6,12 @@ Output: "fl"
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+        return prefix
