ea7a9c17dafd7fc86bc394cb69ef0baeacc3e4a4
Yang Chan Woo <oizys18@gmail.com>
2022-01-02 19:26:19 +0900

    Add leetcode water

---
+++ leetcode/ContainerWithMostWater.py
@@ -0,0 +1,27 @@
+class Solution:
+    # time limit exceeded
+    def maxArea(self, height) -> int:
+        # answer = 0
+        # N = len(height)
+        # for i in range(N):
+        #     for j in range(i+1, N):
+        #         water = abs(j-i)*min(height[j], height[i])
+        #         answer = max(water, answer)
+        # return answer
+        #         answer = 0
+        #         left = 0
+        #         right = 0
+
+        #         for idx,H in enumerate(height):
+        # [1,8,6,2,5,4,8,3,7]
+        # 0,1
+        # 1,8
+        # 2,6
+        # 3,2
+        # 4,5
+        # 5,4
+        # 6,8
+        # 7,3
+        # 8,7
+
+        # 1,8 / 6,8 / 8,7 / 2,6 / 4,5 / 5,4 / 7,3 / 0,1
